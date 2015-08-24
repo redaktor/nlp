ccb2e08a109a250f409423a6030de6bb2328d7f1
redaktor <api@redaktorcms.com>
2015-08-24 20:08:41 +0200

    we will use JSDoc for documentation
    http://usejsdoc.org

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,23 @@
+/** @namespace */
+declare var nlp;
+/**
+ * redaktor/nlp by redaktor foundation and the contributors<br>
+ * derives from<br>
+ * // nlp_comprimise by @spencermountain in 2014<br>
+ * // (https://github.com/spencermountain/nlp_compromise)<br>
+ *
+ * a Natural-Language-Processing library in Javascript, small-enough for the browser, and quick-enough to run on keypress - <br>
+ * it does tons of clever things.
+ * <br><br>
+ * <code>var nlp = new NLP(text, options);<br>nlp.myMethod ...</code>
+ * <br>
+ * @module index
+ * @param {text} text
+ * @param {object} options
+ * @returns {object}
+ * @summary a Natural-Language-Processing library in JS
+ */
+
 // import Verb = require("./verb/verb");
 // import Term = require("./term/term")
 import Sentence = require("./sentence/sentence");
